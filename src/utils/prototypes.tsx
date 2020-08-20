b7eed6c9c84047986664e84df772f90b041ed06e
Jonathan Silva <jonathan.jgs@gmail.com>
2020-08-20 18:17:35 -0300

    adjusting prototypes

--- src/utils/prototypes.tsx
+++ src/utils/prototypes.tsx
@@ -1,8 +1,11 @@
-// eslint-disable-next-line no-extend-native
-Number.prototype.idealDecimalPlaces = function idealDecimalPlaces() {
-  if (this >= 100000) return this.toFixed(1);
-  if (this >= 10000) return this.toFixed(2);
-  if (this >= 10) return this.toFixed(4);
+/*jshint freeze: true */
+Object.defineProperty(Number.prototype, 'idealDecimalPlaces', {
+  value: function () {
+    'use strict';
+    if (this >= 100000) return this.toFixed(1);
+    if (this >= 10000) return this.toFixed(2);
+    if (this >= 10) return this.toFixed(4);
 
-  return this.toFixed(8);
-};
+    return this.toFixed(8);
+  },
+});
